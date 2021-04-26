4251e4986b6e77c8d94ed0e113b7371b14c9ca7e
Greimer29 <greimerabelguzman29@gmail.com>
2021-04-26 14:34:28 -0400

    Simple Sideboard whit icons

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,17 +1,27 @@
 import {Link} from 'react-router-dom';
+import * as FaIcons from 'react-icons/fa';
 
 const Sidebar = function(){
     return(
         <div className="sidebar">
             <ul>
                 <li>
-                    <Link to="/">Inicio</Link>
+                    <Link to="/" className="text-dark"> 
+                        <FaIcons.FaHome className="me-2"/> 
+                        Inicio
+                    </Link>
                 </li>
                 <li>
-                    <Link to="/sales">Ventas</Link>
+                    <Link to="/sales" className="text-dark">
+                        <FaIcons.FaRegChartBar className="me-2"/>
+                        Ventas
+                    </Link>
                 </li>
                 <li>
-                    <Link to="/clients">Clientes</Link>
+                    <Link to="/clients" className="text-dark">
+                        <FaIcons.FaUserFriends className="me-2"/> 
+                        Clientes
+                    </Link>
                 </li>
             </ul>
         </div>
